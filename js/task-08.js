0de714358fc5f371df858a0793957875c11daa7d
Oleksandr Chernomorets <achernomorez@gmail.com>
2022-11-14 15:45:59 +0300

    secComm

--- js/task-08.js
+++ js/task-08.js
@@ -6,14 +6,22 @@ function onFormSubmit(event) {
     event.preventDefault();
 
 
-    
-//    const formElements = event.currentTarget.elements;
-//    const mail = formElements.email.value;
-//    const pass = formElements.password.value;
-//    const formData = {
-//     email: mail,
-//     password: pass,
-//    }
-//    console.log(formData);
 
+   const formElements = event.currentTarget.elements;
+   const mail = formElements.email.value;
+   const pass = formElements.password.value;
+   const formData = {
+    email: mail,
+    password: pass,
+   }
+
+   if(mail.length <= 0 || pass.length <= 0){
+    // console.log(mail.length, pass.length);
+    alert('Заповніть порожні поля');
+    
+   }
+   
+   console.log(formData);
+  
+   formEl.reset()
 }
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+ 
+const changeBtn = document.querySelector('.change-color');
+const textSpan = document.querySelector('.color')
+const bgcBody = document.querySelector('body')
+
+changeBtn.addEventListener('click', onClickChangeBtn);
+
+function onClickChangeBtn() {
+  textSpan.textContent = getRandomHexColor();
+  bgcBody.style.backgroundColor = getRandomHexColor();
 }
